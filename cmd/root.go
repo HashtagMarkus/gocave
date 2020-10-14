7034bef295dd9e1061fccc1ac83943ebc4ca55b6
Markus Hoefer <markus.hoefer@codecentric.de>
2020-10-14 23:08:32 +0200

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,88 @@
+package cmd
+
+import (
+	"debug/pe"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"io"
+	"os"
+)
+
+var cfgFile string
+
+func ioReader(file string) io.ReaderAt {
+	r, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return r
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "gocave",
+	Short: "Find code caves in PE files of given length",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := cobra.MarkFlagRequired(cmd.Flags(), "file")
+		if err != nil {
+			log.Fatal("Missing file flag")
+		}
+
+		fileName, err := cmd.Flags().GetString("file")
+		minCaveSize, err := cmd.Flags().GetInt("size")
+		imageBase, err := cmd.Flags().GetUint32("base")
+
+		file := ioReader(fileName)
+		f, err := pe.NewFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		isAslr := f.Characteristics & pe.IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE
+
+		log.Infof("Looking for code caves in %s", fileName)
+		log.Infof("Image Base: 0x%08x", imageBase)
+		log.Infof("Looking for code caves of minimum %d bytes", minCaveSize)
+		if isAslr > 0 {
+			log.Warn("ASL is enabled. Virtual Address might be different once loaded in memory.")
+		}
+
+		for _, section := range f.Sections {
+			data, err := section.Data()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var count uint32
+			var pos uint32
+			for _, b := range data {
+				if b == 0x00 {
+					count++
+				} else {
+					if count > uint32(minCaveSize) {
+						log.Infof("Cave of %d bytes found in %s at: (Raw Address: 0x%08x, Virtual Address: 0x%08x)", count, section.Name, pos - count, imageBase + section.VirtualAddress + pos - count)
+					}
+					count = 0
+				}
+				pos++
+			}
+		}
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gocave.yaml)")
+
+	rootCmd.Flags().StringP("file", "f", "", "PE File to analyze")
+	rootCmd.Flags().IntP("size", "s", 300, "Minimal size of code cave")
+	rootCmd.Flags().Uint32P("base", "b", 0x00400000, "Base address")
+}
